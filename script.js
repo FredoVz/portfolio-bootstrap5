9d6016c9dd320bc37b163d2e377111ff94049b44
FredoVz <dm.fredo018@gmail.com>
2026-05-29 15:34:44 +0700

    menambahkan animasi tulisan dan gambar pada website

--- script.js
+++ script.js
@@ -25,3 +25,25 @@ form.addEventListener("submit", (e) => {
     })
     .catch((error) => console.error("Error!", error.message));
 });
+
+const galleryImage = document.querySelectorAll(".gallery-img");
+
+//Mengatur animasi gambar gallery
+galleryImage.forEach((img, i) => {
+  img.dataset.aos = "fade-down";
+  img.dataset.aosDelay = i * 100;
+  img.dataset.aosDuration = 1000;
+});
+
+//AOS INIT
+AOS.init({
+  once: true,
+  duration: 2000,
+});
+
+//GSAP
+gsap.registerPlugin(TextPlugin);
+gsap.to(".lead", { duration: 2, delay: 1.5, text: "Programmer | Designer" });
+gsap.from(".jumbotron img", { duration: 1, rotateY: 360, opacity: 0 });
+gsap.from(".navbar", { duration: 1.5, y: "-100%", opacity: 0, ease: "bounce" });
+gsap.from(".display-4", { duration: 1, x: -50, opacity: 0, delay: 0.5, ease: "back" });
